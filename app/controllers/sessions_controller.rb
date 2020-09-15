63f1ab9898a11351c6b199055f45646d1f26d5da
Igor Omelchenko <igor.omelchenko@clark.de>
2020-09-15 09:12:28 +0300

    Added controller tests

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,8 @@ class SessionsController < ApplicationController
   def new; end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
+    user =
+      User.where(role: :admin).find_by(email: params[:session][:email].downcase)
 
     if user&.authenticate(params[:session][:password])
       log_in user
